28dec2a16157f231d8f612c0916bd03dd2c72819
Kamron Mamedov <94013172+ITPrototype@users.noreply.github.com>
2024-01-03 21:19:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'database'
-app.secret_key = 'psichka'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'{os.getenv("DATABASE")}'
+app.secret_key = f'{os.getenv("SECRET_KEY")}'
 app.jinja_env.add_extension('jinja2.ext.do')
 db = SQLAlchemy(app)
 app.config['UPLOAD_FOLDER'] = 'uploads'
